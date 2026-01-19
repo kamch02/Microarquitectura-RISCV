ea5cea8bbda379b0adf35f91b29ff142f589fb08
kamch02 <kamilah.chavarriaa@gmail.com>
2026-01-19 17:53:04 -0600

    Update RegisterFile.v

---
+++ Módulos/RegisterFile.v
@@ -0,0 +1,24 @@
+module reg_file (
+    input  logic        clk,
+    input  logic        RegWrite,
+    input  logic [4:0]  rs1,
+    input  logic [4:0]  rs2,
+    input  logic [4:0]  rd,
+    input  logic [31:0] wd,
+    output logic [31:0] rd1,
+    output logic [31:0] rd2
+);
+
+    logic [31:0] regs [31:0];
+
+    // Read (combinational)
+    assign rd1 = (rs1 == 0) ? 32'b0 : regs[rs1];
+    assign rd2 = (rs2 == 0) ? 32'b0 : regs[rs2];
+
+    // Write (synchronous)
+    always_ff @(posedge clk) begin
+        if (RegWrite && rd != 0)
+            regs[rd] <= wd;
+    end
+
+endmodule
