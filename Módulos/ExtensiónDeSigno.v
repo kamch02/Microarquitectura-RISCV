14f660f80b0840f4c71cf6bade67ff0e442f0d00
kamch02 <kamilah.chavarriaa@gmail.com>
2026-01-19 18:34:08 -0600

    Update VERILOG ExtensiónDeSigno.v

--- Módulos/ExtensiónDeSigno.v
+++ Módulos/ExtensiónDeSigno.v
@@ -1,40 +1,38 @@
 module imm_gen (
-    input  logic [31:0] instr,
-    input  logic [2:0]  ImmSrc,
-    output logic [31:0] imm_ext
+    input  [31:0] instr,
+    input  [2:0]  ImmSrc,
+    output reg [31:0] imm_ext
 );
-
-    always_comb begin
+ 
+    always @(*) begin
         case (ImmSrc)
-
-            3'b000: // I-type
+ 
+            // I-Type (addi, lw, andi, slli)
+            3'b000:
                 imm_ext = {{20{instr[31]}}, instr[31:20]};
-
-            3'b001: // S-type
+ 
+            // S-Type (sw, sb)
+            3'b001:
                 imm_ext = {{20{instr[31]}}, instr[31:25], instr[11:7]};
-
-            3'b010: // B-type
-                imm_ext = {{19{instr[31]}},
-                            instr[31],
-                            instr[7],
-                            instr[30:25],
-                            instr[11:8],
-                            1'b0};
-
-            3'b011: // U-type (lui)
+ 
+            // B-Type (beq, bne, bge)
+            3'b010:
+                imm_ext = {{19{instr[31]}}, instr[31], instr[7],
+                           instr[30:25], instr[11:8], 1'b0};
+ 
+            // U-Type (lui)
+            3'b011:
                 imm_ext = {instr[31:12], 12'b0};
-
-            3'b100: // J-type (jal)
-                imm_ext = {{11{instr[31]}},
-                            instr[31],
-                            instr[19:12],
-                            instr[20],
-                            instr[30:21],
-                            1'b0};
-
+ 
+            // J-Type (jal)
+            3'b100:
+                imm_ext = {{11{instr[31]}}, instr[31],
+                           instr[19:12], instr[20],
+                           instr[30:21], 1'b0};
+ 
             default:
                 imm_ext = 32'b0;
         endcase
     end
-
+ 
 endmodule
