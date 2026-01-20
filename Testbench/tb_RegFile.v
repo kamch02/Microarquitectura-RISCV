3f9f20e6493ca1c96caeb6cd52a52f673baf2259
kamch02 <kamilah.chavarriaa@gmail.com>
2026-01-19 18:51:39 -0600

    Update VERILOG tb_RegFile.v

--- Testbench/tb_RegFile.v
+++ Testbench/tb_RegFile.v
@@ -1,11 +1,13 @@
+`timescale 1ns / 1ps
+ 
 module tb_reg_file;
-
-    logic clk;
-    logic RegWrite;
-    logic [4:0] rs1, rs2, rd;
-    logic [31:0] wd;
-    logic [31:0] rd1, rd2;
-
+ 
+    reg clk;
+    reg RegWrite;
+    reg [4:0] rs1, rs2, rd;
+    reg [31:0] wd;
+    wire [31:0] rd1, rd2;
+ 
     reg_file DUT (
         .clk(clk),
         .RegWrite(RegWrite),
@@ -16,25 +18,21 @@ module tb_reg_file;
         .rd1(rd1),
         .rd2(rd2)
     );
-
+ 
     always #5 clk = ~clk;
-
+ 
     initial begin
         clk = 0;
-
-        // Write register
-        RegWrite = 1;
-        rd = 5'd5;
-        wd = 32'd33;
-        #10;
-
-        // Read register
         RegWrite = 0;
-        rs1 = 5'd5;
+ 
+        rd = 5'd1; wd = 32'd100; RegWrite = 1;
+        #10 RegWrite = 0;
+ 
+        rs1 = 5'd1;
         rs2 = 5'd0;
         #10;
-
+ 
         $stop;
     end
-
+ 
 endmodule
