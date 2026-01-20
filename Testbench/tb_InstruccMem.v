2c1440b964ab9b7a6d37290e11189b82f859b8a5
kamch02 <kamilah.chavarriaa@gmail.com>
2026-01-19 19:01:33 -0600

    Update VERILOG tb_InstruccMem.v

--- Testbench/tb_InstruccMem.v
+++ Testbench/tb_InstruccMem.v
@@ -1,20 +1,49 @@
+`timescale 1ns / 1ps
+ 
 module tb_instr_mem;
-
-    logic [31:0] addr;
-    logic [31:0] instr;
-
+ 
+    reg  [31:0] addr;
+    wire [31:0] instr;
+ 
     instr_mem DUT (
         .addr(addr),
         .instr(instr)
     );
-
+ 
     initial begin
-        addr = 0;   #10;
-        addr = 4;   #10;
-        addr = 8;   #10;
-        addr = 12;  #10;
-
+        // Dirección inicial
+        addr = 32'd0;
+        #10;
+ 
+        // Siguiente instrucción
+        addr = 32'd4;
+        #10;
+ 
+        addr = 32'd8;
+        #10;
+ 
+        addr = 32'd12;
+        #10;
+ 
+        addr = 32'd16;
+        #10;
+ 
+        addr = 32'd20;
+        #10;
+ 
+        // Salto a una dirección más adelante
+        addr = 32'd48;
+        #10;
+ 
+        addr = 32'd52;
+        #10;
+ 
         $stop;
     end
-
+ 
+    initial begin
+        $display("Time\tAddr\t\tInstr");
+        $monitor("%0t\t%h\t%h", $time, addr, instr);
+    end
+ 
 endmodule
