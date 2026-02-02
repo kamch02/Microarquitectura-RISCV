01b1efdd60485134d876c23a4070ac10b838554c
kamch02 <kamilah.chavarriaa@gmail.com>
2026-02-01 22:52:09 -0600

    Se agregan comentarios asociados a funcionalidad

--- Testbench/tb_imm_gen.v
+++ Testbench/tb_imm_gen.v
@@ -1,35 +1,52 @@
-`timescale 1ns / 1ps
- 
+`timescale 1ns/1ps
 module tb_imm_gen;
- 
+
     reg  [31:0] instr;
     reg  [2:0]  ImmSrc;
-    wire [31:0] imm_out;
- 
+    wire [31:0] imm_ext;
+
+    // Instancia del generador de inmediatos
     imm_gen DUT (
         .instr(instr),
         .ImmSrc(ImmSrc),
-        .imm_out(imm_out)
+        .imm_ext(imm_ext)
     );
- 
+
     initial begin
-        instr = 32'h00000013;
-        ImmSrc = 3'b000; // I-type
-        #10;
- 
-        instr = 32'h00F70793; // andi
+        // 
+        // Tipo I (addi)
+        // Verifica extensión de signo correcta
+        // 
+        instr = 32'h02100793;
         ImmSrc = 3'b000;
         #10;
- 
-        instr = 32'hFEF42623; // sw
+
+        // 
+        // Tipo S (sw)
+        // Verifica reconstrucción del inmediato S
+        // 
+        instr = 32'hfef42623;
         ImmSrc = 3'b001;
         #10;
- 
-        instr = 32'hFCE7D0E3; // bge
+
+        // 
+        // Tipo B (bge)
+        // Verifica inmediato PC-relative para branch
+        // 
+        instr = 32'hfce7d0e3;
         ImmSrc = 3'b010;
         #10;
- 
+
+        // 
+        // Tipo J (jal / j)
+        // Verifica inmediato para salto incondicional
+        // 
+        instr = 32'h03c0006f;
+        ImmSrc = 3'b100;
+        #10;
+
         $stop;
     end
- 
 endmodule
+
+
