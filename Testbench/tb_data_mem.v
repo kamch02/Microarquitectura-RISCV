608ad19fe3f64a20001f0bd32ff7bbb1ceba0b55
kamch02 <kamilah.chavarriaa@gmail.com>
2026-02-01 22:54:53 -0600

    Se agregan comentarios asociados a funcionalidad

--- Testbench/tb_data_mem.v
+++ Testbench/tb_data_mem.v
@@ -1,13 +1,14 @@
-`timescale 1ns / 1ps
- 
+`timescale 1ns/1ps
 module tb_data_mem;
- 
+
     reg clk;
     reg MemRead, MemWrite, ByteEn;
     reg [31:0] addr;
     reg [31:0] write_data;
+
     wire [31:0] read_data;
- 
+
+    // Instancia de la memoria de datos
     data_mem DUT (
         .clk(clk),
         .MemRead(MemRead),
@@ -17,24 +18,30 @@ module tb_data_mem;
         .write_data(write_data),
         .read_data(read_data)
     );
- 
+
     always #5 clk = ~clk;
- 
+
     initial begin
         clk = 0;
         MemRead = 0;
         MemWrite = 0;
         ByteEn = 0;
- 
-        addr = 32'd4;
+
+        // 
+        // Escritura de palabra (sw)
+        // 
+        addr = 32'h00000010;
         write_data = 32'h12345678;
         MemWrite = 1;
-        #10 MemWrite = 0;
- 
+        #10;
+        MemWrite = 0;
+
+        // 
+        // Lectura de la palabra escrita
+        // 
         MemRead = 1;
         #10;
- 
+
         $stop;
     end
- 
 endmodule
