0abddb70341fc35cd43b6a8744660a21a7ab65a6
gcascantez <gabocascante24@gmail.com>
2026-02-03 23:56:42 -0600

    Comentarios en instr_mem.v

--- Módulos/instr_mem.v
+++ Módulos/instr_mem.v
@@ -5,15 +5,15 @@ module instr_mem (
     output [31:0] instr
 );
 
-    // 256 words of instruction memory
+    // 256 palabras para la memora de instrucciones
     reg [31:0] mem [0:255];
 
-    // Load program at simulation start
+    // Se lee el archivo .hex que cuenta con las instrucciones obtendias del object dump del código C
     initial begin
         $readmemh("program.hex", mem);
     end
 
-    // Word-aligned access (PC / 4)
+    // Los dos primeros bits son siempre 0, lo relevante es la instrucción que está dada en los bits 9 a 2
     assign instr = mem[addr[9:2]];
 
 endmodule
