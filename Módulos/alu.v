e1d954ed2d793ed6406fe3453b9503d296ce6235
gcascantez <gabocascante24@gmail.com>
2026-02-03 23:53:32 -0600

    Comentarios en la alu.v

--- Módulos/alu.v
+++ Módulos/alu.v
@@ -1,15 +1,21 @@
 `timescale 1ns / 1ps
 
 module alu (
+
+    //Inputs de la ALU
     input  [31:0] A,
     input  [31:0] B,
+    
+    //Selector de función a realizar
     input  [2:0]  ALUCtrl,
+    
+    //Outputs (Resultado y banderas)
     output reg [31:0] Result,
-    output wire Zero,
-    output wire Less
+    output wire Zero,        //BAndera Z
+    output wire Less         //Bandera N
 );
 
-    // ALU combinational logic
+    // Deco para seleccionar función
     always @(*) begin
         case (ALUCtrl)
             3'b000: Result = A + B;        // ADD
@@ -20,7 +26,7 @@ module alu (
         endcase
     end
 
-    // Flags
+    // Banderas
     assign Zero = (Result == 32'b0);
     assign Less = ($signed(A) < $signed(B));
 
